05c10ebeb233c53ab9d0be10e945d52ee4d8bb5e
Ahmad Izzuddin Azzam <diegomortar@gmail.com>
2025-08-11 17:08:12 +0700

    Cors. diperbaiki

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000','https://bersekolah-astro-r2hn9m8ut-blckjackks-projects.vercel.app', 'https://bersekolah-astro.vercel.app/'],
+    'allowed_origins' => [
+        'http://localhost:3000', 
+            'http://127.0.0.1:3000',
+            'https://*.vercel.app',
+            'https://vercel.app',
+        ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://*.vercel.app',
+    ],
 
     'allowed_headers' => ['*'],
 
